dffccf013cbaa24982ea194e281fa872c9ba8db9
Alexandra L LaGrassa <lagrassa@mit.edu>
2015-06-07 06:32:13 -0400

    amin

--- alg.js
+++ alg.js
@@ -3,6 +3,7 @@
 /* data :: [Lat, Long, Score] */
 
 var alg = function(source, target, datapoints){
+    console.log(datapoints);
     var data = datapoints.slice(0);
     solution = [0];
     direction = [target[0]-source[0], target[1]-source[1]];
@@ -56,7 +57,8 @@ var alg = function(source, target, datapoints){
     var w = function(i,j){
         var a = data[i];
         var b = data[j];
-        return (Math.sqrt(Math.pow(b[0]-a[0], 2) + Math.pow(b[1]-a[1], 2))  + error(b)) / (Math.pow(b[2], 10))
+        //return (Math.sqrt(Math.pow(b[0]-a[0], 2) + Math.pow(b[1]-a[1], 2))  
+        return  error(b) / Math.pow(b[2], 2);
     }
     memo = {};
     child = {};
